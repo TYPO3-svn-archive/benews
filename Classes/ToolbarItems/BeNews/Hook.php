04bb2c923585f6b1076c9f339b71ad2f668b4653
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-30 18:32:54 +0000

    [TASK] Fix for 6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/benews/trunk@68153 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ToolbarItems/BeNews/Hook.php
+++ Classes/ToolbarItems/BeNews/Hook.php
@@ -1,4 +1,3 @@
 <?php
-	include_once(t3lib_extMgm::extPath('benews').'Classes/ToolbarItems/BeNews/Item.php');
 	$GLOBALS['TYPO3backend']->addToolbarItem('benews', 'Tx_Benews_ToolbarItems_BeNews_Item');
 ?>
